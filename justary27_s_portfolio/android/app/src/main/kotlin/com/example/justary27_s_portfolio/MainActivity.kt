ecde17aa34abd0840547768d1fef903564545e44
just-ary27 <aryan_r@ch.iitr.ac.in>
2021-08-20 14:24:21 +0530

    Added initial config for flutter based portfolio

---
+++ justary27_s_portfolio/android/app/src/main/kotlin/com/example/justary27_s_portfolio/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.justary27_s_portfolio
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ justary27_s_portfolio/lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:justary27_s_portfolio/src/app.dart';
+
+void main() {
+  runApp(MyApp());
+}
