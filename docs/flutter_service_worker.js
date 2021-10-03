0499b78395c86a32f33606a24092d7d4edb8538a
just-ary27 <aryan_r@ch.iitr.ac.in>
2021-10-03 23:48:55 +0530

    Update to r

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -17,9 +17,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "20b4f564083707ba4eb18810b28750d0",
-"/": "20b4f564083707ba4eb18810b28750d0",
-"main.dart.js": "cebdd702d7f31345a4d9c214e652314f",
+"index.html": "622f5febb946d24235e0d2b276b34268",
+"/": "622f5febb946d24235e0d2b276b34268",
+"main.dart.js": "6a20b9ab9dba395a4a2133254a1fc320",
 "manifest.json": "cb66442964301fef6b15d7371bc6e01d",
 "version.json": "306aa6742d7d65caea82e27331d8c466"
 };
