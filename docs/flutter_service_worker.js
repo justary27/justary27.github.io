d3c20e5c09a460d809bdb1243ce80059847df908
just-ary27 <aryan_r@ch.iitr.ac.in>
2021-10-04 23:02:44 +0530

    Update to fix the site

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -3,9 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "assets/AssetManifest.json": "865dd566e73cf9e0e2e05775cf873ade",
+  "assets/AssetManifest.json": "04ff38ceb1d565735811904ce04ee3c1",
 "assets/assets/fonts/Allison-Regular.ttf": "237a9ce39570240f90f04848e68e5bfc",
-"assets/FontManifest.json": "1b94b23b36c857bd168c1bd3a219b753",
+"assets/assets/fonts/DjangoButton.ttf": "6fd04ea28f8318c09682f67d3b1b0165",
+"assets/assets/fonts/HomeButton.ttf": "ea182ee83ed070fff9436ee8a1d59c93",
+"assets/FontManifest.json": "d7894163e3d5df33f145ad5f77eb77fd",
 "assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
 "assets/NOTICES": "a589129bd4eff76e336596a8c99c97f5",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
@@ -17,9 +19,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "1b8334a9cf4088cd5348b77ee9598f33",
-"/": "1b8334a9cf4088cd5348b77ee9598f33",
-"main.dart.js": "b640b4ceafef38dde4e1306f5ef775cb",
+"index.html": "8a3fd47c328c4c4bde23a1ccc2b1933b",
+"/": "8a3fd47c328c4c4bde23a1ccc2b1933b",
+"main.dart.js": "f59381c2173328ab071ad8716092bd46",
 "manifest.json": "cb66442964301fef6b15d7371bc6e01d",
 "version.json": "306aa6742d7d65caea82e27331d8c466"
 };
